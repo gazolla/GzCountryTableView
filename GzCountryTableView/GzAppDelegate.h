3b81bb4ba8ce365ed525ed8391a3ba9128ae2cb0
Sebastiao Gazolla jr <gazolla@gmail.com>
2013-05-02 16:26:07 -0300

    Initial Commit

---
+++ GzCountryTableView/GzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GzAppDelegate.h
+//  GzCountryTableView
+//
+//  Created by Gazolla on 02/05/13.
+//  Copyright (c) 2013 Gazolla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GzCountryTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GzCountryTableView
+//
+//  Created by Gazolla on 02/05/13.
+//  Copyright (c) 2013 Gazolla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GzAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GzAppDelegate class]));
+    }
+}
